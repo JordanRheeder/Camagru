c105cfb8ffe875b77a40ac869429658712b9bdcc
jordanrheeder <jordanrheeder@gmail.com>
2019-10-31 10:14:34 +0200

    removed changed port 8080 to 80, don't want to type the port number everytime.

--- config/connect.php
+++ config/connect.php
@@ -3,8 +3,6 @@
 	try {
 		$dbh = new PDO($dsn.";dbname=db_camagru", $db_user, $db_passwd);
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// $dbh->exec("CREATE DATABASE IF NOT EXISTS $db_name");
-		// $quer->exec($quer);
 	}
 	catch(PDOException $e) {
 		echo "ERROR: ".$e->getMessage();
