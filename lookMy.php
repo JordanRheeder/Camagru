2967ed246d3aa364d6d11e661c232d14e75c3500
Jordan Rheeder <jordanrheeder@gmail.com>
2019-11-05 16:49:09 +0200

    lookAll, is just a temporary name, but essentially only has like post and comment feature active. However, lookMy, allows for liking(who likes their own post anyways?), commenting and DELETING their post(s)

---
+++ lookMy.php
@@ -0,0 +1,177 @@
+<?php
+	ini_set("display_errors", true);
+	include ("config/db_setup.php");
+	if (session_id() === "") {
+		session_start();
+	}
+	else {
+		$session_id=session_id();
+	}
+	// include("functions/functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Camagru</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+	</head>
+<body>
+	<!-- <div class="main_wrapper"> -->
+		<nav class="navbar" role="navigation" aria-label="main navigation">
+			<div class="navbar-brand">
+				<a class="navbar-item" href="index.php">
+					<img src="images/final.gif" width="112px" height="112px">
+				</a>
+				<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+			<div id="navbarBasicExample" class="navbar-menu">
+				<div class="navbar-start">
+					<a class="navbar-item" href="index.php">
+						Home
+					</a>
+			<?php
+				if(isset($_SESSION['user_email'])) {
+					// echo "<a href='index.php?Logout=TRUE'>Logout</a>";
+					echo "<a class='navbar-item' href='my_account.php'>My Account</a>";
+				}
+				else {
+					echo "";
+				}
+			?>
+			<div class="navbar-item">
+			<?php
+				if(!isset($_SESSION['user_email'])) {
+					echo "";
+				}
+				else {
+					echo "</a>";
+					echo "<a class='navbar-item' href='post-img.php'>";
+					echo "Post";
+					echo "</a>";
+					if (isset($_SESSION['user_email'])) {
+						echo "<a class='navbar-item' href='view-posts.php'>My Posts</a>";
+					}
+					if (isset($_SESSION['user_email'])) {
+						echo "<a class='navbar-item' href='view-posts-all.php'>All Posts</a>";
+					}
+				}
+			?>
+
+				</div>
+			</div>
+		</div>
+			<div class="navbar-end">
+				<div class="navbar-item">
+					<div class="buttons">
+						<?php
+							if(isset($_SESSION['user_email'])) {
+								echo "";
+							}
+							else {
+								echo "<a class='button is-primary' href='register.php'>";
+								echo "<strong>Sign up</strong>";
+								echo "</a>";
+							}
+						?>
+						<?php
+						if(isset($_SESSION['user_email'])) {
+							echo "<a class='button is-light' href='login.php?Logout=TRUE'>Log out</a>";
+						}
+						else {
+								echo "<a class='button is-light' href='login.php'>Log in</a>";
+						}
+						?>
+
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<?php
+			if (isset($_GET['img'])) {
+				$img = $_GET['img'];
+				echo "<img id='myImg' src='users/post_images/$img' alt='$img' style='width:100%;max-width:300px'>";
+			}
+		?>
+		<div class="buttons">
+			<!-- Query the db to see who owns this pic -->
+			<!-- If it's true then show delete post button ELSE don't -->
+			<!-- Create like and comments -->
+			<?php
+				if (isset($_GET['img'])) {
+					$img = $_GET['img'];
+				}
+				echo "<a class='button is-primary' href='lookMy.php?Like=TRUE&img=$img'>Like Post</a>";
+				if (!isset($_GET['img'])) {
+					echo "";
+				}
+				else {
+					// query DB FUNCTION
+					echo "<a class='button is-primary' href='lookMy.php?Delete=TRUE&img=$img'>Delete Post</a>";
+				}
+
+			?>
+			<!-- <a class='button is-primary' href='look.php'>Delete Post</a> -->
+		</div>
+		<!--footer starts-->
+		<div id="footer">
+			<h2 style="text-align:center; padding-top:30px;">jrheeder</h2>
+		</div>
+		<!--footer ends-->
+		<!-- Scripts -->
+		<script>document.addEventListener('DOMContentLoaded', () => {
+			// Get all "navbar-burger" elements
+			const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+			// Check if there are any navbar burgers
+			if ($navbarBurgers.length > 0) {
+				// Add a click event on each of them
+				$navbarBurgers.forEach( el => {
+					el.addEventListener('click', () => {
+					// Get the target from the "data-target" attribute
+						const target = el.dataset.target;
+						const $target = document.getElementById(target);
+						// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+						el.classList.toggle('is-active');
+						$target.classList.toggle('is-active');
+
+					});
+				});
+			}
+		});
+		</script>
+		<!-- Scripts end -->
+
+
+  </body>
+</html>
+<?php
+	include('includes/functions.php');
+	if (isset($_GET['Logout'])) {
+		if ($_GET['Logout'] == 'TRUE')
+			session_destroy();
+		echo "<script>window.open('index.php', '_self')</script>";
+	}
+	if (isset($_GET['Like']))
+	{
+		if ($_GET['Like'] == 'TRUE') {
+		// LIKE POST FUNCTION (ALSO CHECK IF THIS USER HAS LIKED THIS POST IF YES, UNLIKE)
+		}
+	}
+	if (isset($_GET['Delete'])) {
+		if ($_GET['Delete'] == 'TRUE') {
+			// DELETE POST FUNCTION
+			$userEmail = $_SESSION['user_email'];
+			$img = $_GET['img'];
+			echo $img;
+			deletePost($userEmail, $img);
+			echo "<script>window.open('index.php', '_self')</script>";
+		}
+	}
+?>
