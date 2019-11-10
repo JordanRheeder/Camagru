a22e06918024ee846060252d3e6b51bb4593b228
Jordan Rheeder <JordanRheeder@gmail.com>
2019-11-10 21:52:04 +0200

    Removed comments, redesigned navbar, removed useless code.

--- register.php
+++ register.php
@@ -12,27 +12,22 @@ session_start();
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 	</head>
 <body>
-	<!-- <div class="main_wrapper"> -->
 		<nav class="navbar" role="navigation" aria-label="main navigation">
-		  <div class="navbar-brand">
-		    <a class="navbar-item" href="index.php">
-		      <img src="images/final.gif" width="112px" height="112px">
-		    </a>
+			<div class="navbar-brand">
+				<a class="navbar-item" href="index.php">
+					<img src="images/final.gif" width="112px" height="112px">
+				</a>
 
-		    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-		      <span aria-hidden="true"></span>
-		      <span aria-hidden="true"></span>
-		      <span aria-hidden="true"></span>
-		    </a>
-		  </div>
+				<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				</a>
+			</div>
 
-		  <div id="navbarBasicExample" class="navbar-menu">
-		    <div class="navbar-start">
-		      <a class="navbar-item" href="index.php">
-		        Home
-		      </a>
-
-
+			<div id="navbarBasicExample" class="navbar-menu">
+				<div class="navbar-start">
+					<a class="navbar-item" href="index.php">Home</a>
 			<?php
 				if(isset($_SESSION['user_email'])) {
 					// echo "<a href='index.php?Logout=TRUE'>Logout</a>";
@@ -42,21 +37,18 @@ session_start();
 					echo "";
 				}
 			?>
-
 		</div>
 		<div class="navbar-end">
 		<div class="navbar-item">
 		<div class="buttons">
-			            <?php
-							if(isset($_SESSION['user_email'])) {
-								echo "";
-							}
-							else {
-								echo "<a class='button is-primary' href='register.php'>";
-								echo "<strong>Sign up</strong>";
-								echo "</a>";
-							}
-						?>
+			<?php
+				if(isset($_SESSION['user_email'])) {
+					echo "";
+				}
+				else {
+					echo "<a class='button is-primary' href='register.php'><strong>Sign up</strong></a>";
+				}
+			?>
 		<?php
 			if(isset($_SESSION['user_email'])) {
 				echo "<a class='button is-light' href='login.php?Logout=TRUE'>Log out</a>";
@@ -70,45 +62,45 @@ session_start();
 		</div>
 		</div>
 		</nav>
-
 		<!--content wrapper starts-->
-		<div class="content_wrapper" align="center">
-		<form action="register.php" method="post" enctype="multipart/form-data" align="center">
-					<table align="center">
-            <tr>
-							<td align="right" style="color: white">......</td>
-							<td><input placeholder="Username"type="text" name="username" required></td>
-						</tr>
-            <tr>
-							<td align="right" style="color: white">......</td>
-							<td><input placeholder="E-mail"type="text" name="email" required></td>
-						</tr>
-						<tr>
-							<td align="right" style="color: white">.</td>
-							<td><input placeholder="Password" type="password" name="user_passwd" required/></td>
-						</tr>
-						<tr>
-							<td align="right" style="color: white">First Name:</td>
-							<td><input placeholder="First name" type="text" name="firstname" required/></td>
-						</tr>
 
-						<tr>
-							<td align="right" style="color: white">Surname:</td>
-							<td><input placeholder="Surname" type="text" name="surname" required/></td>
-						</tr>
-						<tr>
-							<td align="right" style="color: white">Profile Photo:</td>
-							<!-- <td><input type="file" name="profilePhoto" required/></td> ADD THIS IN LATER************************ -->
-							<td><input placeholder="Profile Photo" type="file" name="profilePhoto"/></td>
-						</tr>
-						<tr>
-							<td align="right" style="color: white">Contact:</td>
-							<td><input placeholder="Phone Number" type="text" name="PhoneNumber" required/></td>
-						</tr>
-				</table>
-					<tr align="right">
-						<td><input class="button is-primary" type="submit" name="register" value="Create Account" style="margin-left: -8px; align:center"/></td>
+		<div class="content_wrapper" style="align: center">
+			<form action="register.php" method="post" enctype="multipart/form-data" align="center">
+				<table align="center">
+					<tr>
+						<td align="right" style="color: white">......</td>
+						<td><input placeholder="Username"type="text" name="username" required></td>
 					</tr>
+					<tr>
+						<td align="right" style="color: white">......</td>
+						<td><input placeholder="E-mail"type="text" name="email" required></td>
+					</tr>
+					<tr>
+						<td align="right" style="color: white">.</td>
+						<td><input placeholder="Password" type="password" name="user_passwd" required/></td>
+					</tr>
+					<tr>
+						<td align="right" style="color: white">First Name:</td>
+						<td><input placeholder="First name" type="text" name="firstname" required/></td>
+					</tr>
+
+					<tr>
+						<td align="right" style="color: white">Surname:</td>
+						<td><input placeholder="Surname" type="text" name="surname" required/></td>
+					</tr>
+					<tr>
+						<td align="right" style="color: white">Profile Photo:</td>
+						<!-- <td><input type="file" name="profilePhoto" required/></td> ADD THIS IN LATER************************ -->
+						<td><input placeholder="Profile Photo" type="file" name="profilePhoto" required/></td>
+					</tr>
+					<tr>
+						<td align="right" style="color: white">Contact:</td>
+						<td><input placeholder="Phone Number" type="text" name="PhoneNumber" required/></td>
+					</tr>
+			</table>
+			<tr align="right">
+				<td><input class="button is-primary" type="submit" name="register" value="Create Account" style="margin-left: -8px; align:center"/></td>
+			</tr>
 		</form>
 		</div>
 		<!--content wrapper ends-->
@@ -164,49 +156,49 @@ session_start();
 
 	if (isset($_POST['register'])) {
 		try {
-			$passwd = hash('whirlpool',$_POST['user_passwd']);
-			$username = $_POST['username'];
-			$firstname = $_POST['firstname'];
-			$surname = $_POST['surname'];
-			$img = $_FILES['profilePhoto']['name'];
-			$image_tmp = $_FILES['profilePhoto']['tmp_name'];
-			$contact = $_POST['PhoneNumber'];
-			$email = strtolower($_POST['email']);
-			$token = hash('md5', $email);
-			$verified = 0;
-			move_uploaded_file($image_tmp, "users/user_images/$img");
-			$query = "INSERT INTO `users` (user_passwd, user_firstname, user_surname, user_email, username, user_contact, user_image, token, verified) VALUES (?,?,?,?,?,?,?,?,?)";
-      $query = $dbh->prepare($query);
-
-			//********* checking the database for existing emails or users *********
-			$verifySQL = ("SELECT user_email FROM `users` WHERE user_email=:user_email");
-			$verify = $dbh->prepare($verifySQL);
-			$verify->bindParam(':user_email', $email, PDO::PARAM_STR);
-			$verify->execute();
-			$row = $verify->fetch();
-			$check_email  = $row['user_email'];
-
-			if (empty($row['user_email'])) {
-			$query->bindParam('1', $passwd);
-			$query->bindParam('2', $firstname);
-			$query->bindParam('3', $surname);
-			$query->bindParam('4', $email);
-      $query->bindParam('5', $username);
-			$query->bindParam('6', $contact);
-			$query->bindParam('7', $img);
-			$query->bindParam('8', $token);
-			$query->bindParam('9', $verified);
-			$query->execute();
-				if (mailVerifCode($email, $token, $firstname))
-					echo "<script>alert('Account created!');</script>";
+			if (passwordStrength($_POST['user_passwd']) === 1) {
+				$passwd = hash('whirlpool',$_POST['user_passwd']);
+				$username = $_POST['username'];
+				$firstname = $_POST['firstname'];
+				$surname = $_POST['surname'];
+				$img = $_FILES['profilePhoto']['name'];
+				$image_tmp = $_FILES['profilePhoto']['tmp_name'];
+				$contact = $_POST['PhoneNumber'];
+				$email = strtolower($_POST['email']);
+				$token = hash('md5', $email);
+				$verified = 0;
+				move_uploaded_file($image_tmp, "users/user_images/$img");
+				$query = "INSERT INTO `users` (user_passwd, user_firstname, user_surname, user_email, username, user_contact, user_image, token, verified) VALUES (?,?,?,?,?,?,?,?,?)";
+				$query = $dbh->prepare($query);
+				//********* checking the database for existing emails or users *********
+				$verifySQL = ("SELECT user_email FROM `users` WHERE user_email=:user_email");
+				$verify = $dbh->prepare($verifySQL);
+				$verify->bindParam(':user_email', $email, PDO::PARAM_STR);
+				$verify->execute();
+				$row = $verify->fetch();
+				$check_email  = $row['user_email'];
+				if (empty($row['user_email'])) {
+				$query->bindParam('1', $passwd);
+				$query->bindParam('2', $firstname);
+				$query->bindParam('3', $surname);
+				$query->bindParam('4', $email);
+				$query->bindParam('5', $username);
+				$query->bindParam('6', $contact);
+				$query->bindParam('7', $img);
+				$query->bindParam('8', $token);
+				$query->bindParam('9', $verified);
+				$query->execute();
+					if (mailVerifCode($email, $token, $firstname))
+						echo "<script>alert('Account created!');</script>";
+				}
+				else
+					echo "<script>alert('You already have an account!');</script>";
+				}
 			}
-			else
-				echo "<script>alert('You already have an account!');</script>";
-		}
-		catch(PDOException $e) {
-			echo "ERROR: ".$e->getMessage();
-			exit(2);
-		}
+			catch(PDOException $e) {
+				echo "ERROR: ".$e->getMessage();
+				exit(2);
+			}
 	}
 
 	if (isset($_GET['token'])) {
